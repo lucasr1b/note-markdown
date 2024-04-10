d404676b65dd218754ec46dedf84b7d923ca8612
lucasr1b <lucascamribeiro@gmail.com>
2024-04-10 15:34:05 +1200

    If user is in selected note and they delete it, redirect to notes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { TrashIcon } from '@heroicons/react/16/solid';
 import axios from 'axios';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface SidebarProps {
   notes: string[];
@@ -9,27 +9,33 @@ interface SidebarProps {
 }
 
 const Sidebar = (props: SidebarProps) => {
+
+  const { push } = useRouter();
+
+  const getSelectedNoteId = () => {
+    return window.location.pathname.split('/').pop();
+  }
+
   const newNote = async () => {
     const note = await axios.post('/api/notes', { userId: '1' });
     props.setNotes([...props.notes, note.data]);
   }
 
   const deleteNote = async (e: any, noteId: string) => {
+    if (getSelectedNoteId() === noteId) push('/notes');
     e.preventDefault();
     props.setNotes(props.notes.filter((note: any) => note._id !== noteId));
     await axios.delete(`/api/notes/${noteId}`);
   }
 
-  const getSelectedNoteId = () => {
-    return window.location.pathname.split('/').pop();
-  }
+
 
   return (
     <ul className='menu fixed w-56 h-full bg-base-200 border-r-2 border-base-100 gap-1'>
-      <div className='p-2 flex items-center gap-2'>
+      <Link className='p-2 flex items-center gap-2' href={'/notes'}>
         <div className='bg-base-300 rounded-lg py-1 px-2 w-fit'>NM</div>
         <h1 className='font-bold'>NoteMarkdown</h1>
-      </div>
+      </Link>
       <li className='btn btn-sm h-10 btn-primary mt-2 mb-8 no-animation' onClick={newNote}>
         Create new note
       </li>
