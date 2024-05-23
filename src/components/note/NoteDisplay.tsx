6f2422fa0ef966bdef424101abdb557f62c5fc6b
lucasr1b <lucascamribeiro@gmail.com>
2024-05-23 12:20:58 +1200

    Added syntax highlighting, contents and content navigation

--- src/components/note/NoteDisplay.tsx
+++ src/components/note/NoteDisplay.tsx
@@ -1,5 +1,9 @@
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeSlug from 'rehype-slug';
+import remarkToc from 'remark-toc';
+import 'highlight.js/styles/atom-one-dark.css';
 
 type NoteDisplayProps = {
   title: string;
@@ -7,10 +11,18 @@ type NoteDisplayProps = {
 };
 
 const NoteDisplay = (props: NoteDisplayProps) => {
+
+  document.querySelector('#content');
+
   return (
     <>
       <p className='text-4xl font-bold mb-1'>{props.title}</p>
-      <div className='note-display bg-inherit'><Markdown remarkPlugins={[remarkGfm]}>{props.content}</Markdown></div>
+      <div className='note-display bg-inherit'>
+        <Markdown
+          rehypePlugins={[rehypeHighlight, rehypeSlug]}
+          remarkPlugins={[remarkGfm, remarkToc]}>{props.content}
+        </Markdown>
+      </div>
     </>
   );
 };
