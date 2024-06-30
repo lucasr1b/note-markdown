0820240c0a25298713a1110b2d93b0ec1dbe54f1
lucasr1b <lucascamribeiro@gmail.com>
2024-07-01 08:08:14 +1200

    Minor login and sign up responsive fixes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -22,10 +22,10 @@ const SignupPage = () => {
   return (
     <div className='relative h-screen'>
       <Link className='absolute top-0 left-0 p-4' href={'/'}>
-        <h1 className='text-2xl font-bold'>NoteMarkdown</h1>
+        <h1 className='text-xl font-bold sm:text-2xl'>NoteMarkdown</h1>
       </Link>
       <div className='bg-base-300 flex flex-col items-center justify-center h-full w-full'>
-        <h1 className='text-4xl font-extrabold text-center mb-2'>Edit and view markdown</h1>
+        <h1 className='text-4xl mt-6 font-extrabold text-center mb-2'>Edit and view markdown</h1>
         <p className='text-content-accent mb-2 sm:mb-6'>Sign up now for free!</p>
         <SignupForm />
         <Link href={'/login'} className='text-secondary underline mt-4'>
